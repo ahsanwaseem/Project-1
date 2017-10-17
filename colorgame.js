248d974bd7a52c73b49c9e38219df6f1d139c185
phpradio <allphpeverything@gmail.com>
2017-10-17 01:08:22 -0700

    added game modes

--- colorgame.js
+++ colorgame.js
@@ -1,14 +1,45 @@
-var colors = generateRandomColors(6);
-
-var squares = document.querySelectorAll('.square');
-var pickedColor = pickColor();
-var colorDisplay = document.getElementById('colorDisplay');
+var numofSquares   = 6;
+var colors         = generateRandomColors(numofSquares);
+var squares        = document.querySelectorAll('.square');
+var pickedColor    = pickColor();
+var colorDisplay   = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
-var h1 = document.querySelector('h1');
-var resetButton = document.querySelector('#reset');
+var h1             = document.querySelector('h1');
+var resetButton    = document.querySelector('#reset');
+var easyBtn        = document.querySelector('#easybtn');
+var hardBtn        = document.querySelector('#hardbtn');
+
+
+easyBtn.addEventListener('click',function(){
+  hardBtn.classList.remove('selected');
+  easyBtn.classList.add('selected');
+  numofSquares = 3;
+  colors = generateRandomColors(numofSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i =0; i < squares.length; i++){
+    if(colors[i]){
+      squares[i].style.backgroundColor = colors[i];
+    }else{
+      squares[i].style.display = 'none';
+    }
+  }
+});
+hardBtn.addEventListener('click',function(){
+  hardBtn.classList.add('selected');
+  easyBtn.classList.remove('selected');
+  numofSquares = 6;
+  colors = generateRandomColors(numofSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for(var i =0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = 'block';
+  }
+});
 
 resetButton.addEventListener('click',function(){
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numofSquares);
   pickedColor = pickColor();
   colorDisplay.textContent = pickedColor;
 
