0ee73ae5b36434839f8775980563fa80bf398772
phpradio <allphpeverything@gmail.com>
2017-10-16 23:54:13 -0700

    color game

--- colorgame.js
+++ colorgame.js
@@ -4,6 +4,19 @@ var squares = document.querySelectorAll('.square');
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector('h1');
+var resetButton = document.querySelector('#reset');
+
+resetButton.addEventListener('click',function(){
+  colors = generateRandomColors(6);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+
+  for (var i=0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = '#232323';
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -13,7 +26,9 @@ for(var i=0; i < squares.length; i++){
     var clickedColor = this.style.backgroundColor;
     if(clickedColor === pickedColor){
       messageDisplay.textContent = 'Correct';
+      resetButton.textContent = 'Play Again?';
       changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
     }else{
       this.style.backgroundColor = '#232323';
       messageDisplay.textContent = 'Try Again';
@@ -44,5 +59,5 @@ function randomColor(){
   var r = Math.floor(Math.random() * 256);
   var g = Math.floor(Math.random() * 256);
   var b = Math.floor(Math.random() * 256);
-  return 'rgb(' + r + ',' + g + ',' + b + ')';
+  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
